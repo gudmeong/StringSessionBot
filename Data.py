68b2cd97b8694ed9d8d95896048031303156fa7e
gudmeong <privatemymail758@gmail.com>
2022-05-02 07:21:00 +0700

    Support Pyrogram v2 and excep blocks for pyrogram v1!
    Signed-off-by: gudmeong <privatemymail758@gmail.com>

--- Data.py
+++ Data.py
@@ -2,31 +2,17 @@ from Config import BOT_USERNAME
 from pyrogram.types import InlineKeyboardButton
 
 class Data:
-    # Start Message
-    START = """
-Hey {}
-
-I am {}
-
-Calm Down, i am isn't suscipious.
-1. Not logging your api, session, even your phone number
-2. Base on me is open source code😊, you can full read a message!
-
-You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
-    """
-    # Home Button
+    generate_single_button = [InlineKeyboardButton("Start Generating Session", callback_data="generate")]
     home_buttons = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")],
+        generate_single_button,
         [InlineKeyboardButton(text="Back", callback_data="home")]
     ]
 
-    generate_button = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")]
-    ]
+    generate_button = [generate_single_button]
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("Start Generating Session", callback_data="generate")],
+        generate_single_button,
         [InlineKeyboardButton("Bot Alive Time", callback_data="stats_callback")],
         [
             InlineKeyboardButton("How to Use ❔", callback_data="help"),
@@ -34,6 +20,13 @@ You can use me to generate pyrogram and telethon string session. Use below butto
         ],
     ]
 
+    START = f"""Hey {}
+I am {}
+Calm Down, i am isn't suscipious.
+1. Not logging your api, session, even your phone number
+2. Base on me is open source code😊, you can full read a message!
+You can use me to generate pyrogram and telethon string session. Use below buttons to learn more !
+    """
     # Help Message
     HELP = """
  **Available Commands** 
@@ -41,7 +34,7 @@ You can use me to generate pyrogram and telethon string session. Use below butto
 /about - About The Bot
 /help - This Message
 /start - Start the Bot
-/generate - Start Generating Session
+/generate - Generate Session
 /cancel - Cancel the process
 /restart - Cancel the process
 /ping - Checking Latency of bot
